714da55d6d71f87e727db336158fb43cb15ed743
Math0XK <145477623+Math0XK@users.noreply.github.com>
2025-08-15 11:53:02 +0200

    Switch OLED library to Adafruit SH110X and add demos
    Replaced U8g2 with Adafruit SH110X for OLED display, updated initialization and drawing routines, and added multiple display demo functions (lines, shapes, text, bitmaps) to OLEDManager. Updated platformio.ini to use Adafruit SH110X library. Added .gitignore and VSCode extension recommendations.

--- ESP32-S3 Korvo 2/src/OLEDManager/OLEDManager.hpp
+++ ESP32-S3 Korvo 2/src/OLEDManager/OLEDManager.hpp
@@ -1,16 +1,44 @@
 #ifndef OLEDMANAGER_HPP
 #define OLEDMANAGER_HPP
-
-#include <U8g2lib.h>
 #include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
 
-extern U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2;
+#define i2c_Address 0x3c
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET -1   //   QT-PY / XIAO
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
+
+#define LOGO_HEIGHT 64
+#define LOGO_WIDTH 128
+
+extern const unsigned char PROGMEM logo16_glcd_bmp[];
+extern const unsigned char PROGMEM myBitmap [];
+
+extern Adafruit_SH1106G display;
+
+extern void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h);
+extern void testdrawchar(void);
+extern void testdrawcircle(void);
+extern void testfillrect(void);
+extern void testdrawtriangle(void);
+extern void testfilltriangle(void);
+extern void testdrawroundrect(void);
+extern void testfillroundrect(void);
+extern void testdrawrect(void);
+extern void testdrawline();
 
 
 
-extern void setupOLED();
-extern void setSH1106Offset(uint8_t offset);
-extern void drawText(const char* text);
 
 
 #endif // OLEDMANAGER_HPP
--- ESP32-S3 Korvo 2/src/main.cpp
+++ ESP32-S3 Korvo 2/src/main.cpp
@@ -1,20 +1,111 @@
 #include "main.hpp"
 
-
 void setup() {
 	Serial.begin(115200);
 	Serial.println("Hello from Korvo !");
+    Wire.begin(11, 12);
 	spiffsBegin();
 	startWiFiAP();
 	setupServer();
-	setupOLED();
-	setSH1106Offset(64);
-	drawText("Waffle ready!");
+	delay(250); // wait for the OLED to power up
+  	display.begin(i2c_Address, true); 
+	display.display();
+	delay(2000);
+	display.drawPixel(10, 10, SH110X_WHITE);
+	// Show the display buffer on the hardware.
+	// NOTE: You _must_ call display after making any drawing commands
+	// to make them visible on the display hardware!
+	display.display();
+	delay(2000);
+	display.clearDisplay();
 
-	// Wait for a moment to ensure everything is set up before proceeding
+	// draw many lines
+	testdrawline();
+	display.display();
+	delay(2000);
+	display.clearDisplay();
+
+	// draw rectangles
+	testdrawrect();
+	display.display();
+	delay(2000);
+	display.clearDisplay();
+
+	// draw multiple rectangles
+	testfillrect();
+	display.display();
+	delay(2000);
+	display.clearDisplay();
+
+	// draw mulitple circles
+	testdrawcircle();
+	display.display();
+	delay(2000);
+	display.clearDisplay();
+
+	// draw a SH110X_WHITE circle, 10 pixel radius
+	display.fillCircle(display.width() / 2, display.height() / 2, 10, SH110X_WHITE);
+	display.display();
+	delay(2000);
+	display.clearDisplay();
+
+	testdrawroundrect();
+	delay(2000);
+	display.clearDisplay();
+
+	testfillroundrect();
+	delay(2000);
+	display.clearDisplay();
+
+	testdrawtriangle();
+	delay(2000);
+	display.clearDisplay();
+
+	testfilltriangle();
+	delay(2000);
+	display.clearDisplay();
+
+	// draw the first ~12 characters in the font
+	testdrawchar();
+	display.display();
+	delay(2000);
+	display.clearDisplay();
+
+
+
+	// text display tests
+	display.setTextSize(1);
+	display.setTextColor(SH110X_WHITE);
+	display.setCursor(0, 0);
+	display.println("Failure is always an option");
+	display.setTextColor(SH110X_BLACK, SH110X_WHITE); // 'inverted' text
+	display.println(3.141592);
+	display.setTextSize(2);
+	display.setTextColor(SH110X_WHITE);
+	display.print("0x"); display.println(0xDEADBEEF, HEX);
+	display.display();
+	delay(2000);
+	display.clearDisplay();
+
+	// miniature bitmap display
+	// display.drawBitmap(30, 16,  logo16_glcd_bmp, 16, 16, 1);
+	display.drawBitmap(0, 0, myBitmap, 128, 64, 1);
+	display.display();
+	delay(10000);
+
+	// invert the display
+	display.invertDisplay(true);
 	delay(1000);
+	display.invertDisplay(false);
+	delay(1000);
+	display.clearDisplay();
+
+	// draw a bitmap icon and 'animate' movement
+	testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
+
 }
 
 void loop() {
 
 }
+
